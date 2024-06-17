e2fe26f9430667d5b56e97b3d5643d535683f7c7
Kim Wongi <woongi3012@gmail.com>
2024-06-17 17:36:10 +0900

    chore : 주석 삭제 style : CSS -> styled-components

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,24 +1,3 @@
-// import { Suspense, lazy } from "react";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import "./index.css";
-// import RouteChangeTracker from "./utils/routeChangeTracker";
-
-// const Login = lazy(() => import("./Pages/Login"));
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//         <Suspense>
-//           <Routes>
-//             <Route path="/" element={<Login />} />
-//           </Routes>
-//         </Suspense>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import Login from "./Pages/Login";
 
 function App() {
