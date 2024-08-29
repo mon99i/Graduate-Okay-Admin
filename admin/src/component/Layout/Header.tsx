52b2b7c31d1316076e7cfa6a5cb8f261c25c3670
Kim Wongi <woongi3012@gmail.com>
2024-08-29 21:52:26 +0900

    style: header 스타일 수정

--- admin/src/component/Layout/Header.tsx
+++ admin/src/component/Layout/Header.tsx
@@ -12,12 +12,17 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="bg-gray-800 text-white px-4 py-2 flex justify-center space-x-16">
-      <button onClick={() => navigate('/notice')} className='hover:text-gray-400'>공지사항 관리</button>
-      <button onClick={() => navigate('/lecture')} className='hover:text-gray-400'>강의 관리</button>
-      <button onClick={() => navigate('/review')} className='hover:text-gray-400'>리뷰 관리</button>
-      <button onClick={() => navigate('/user')} className='hover:text-gray-400'>사용자 관리</button>
-      <button onClick={() => navigate('/admin')} className='hover:text-gray-400'>관리자 관리</button>
+    <header className="bg-gray-800 text-white px-4 py-2 flex justify-center space-x-8">
+      <button onClick={() => navigate('/notice')} className='hover:text-gray-400'>공지사항</button>
+      <span className="text-gray-400 text-sm mx-1">|</span>
+      <button onClick={() => navigate('/lecture')} className='hover:text-gray-400'>강의</button>
+      <span className="text-gray-400 text-sm mx-1">|</span>
+      <button onClick={() => navigate('/review')} className='hover:text-gray-400'>리뷰</button>
+      <span className="text-gray-400 text-sm mx-1">|</span>
+      <button onClick={() => navigate('/user')} className='hover:text-gray-400'>사용자</button>
+      <span className="text-gray-400 text-sm mx-1">|</span>
+      <button onClick={() => navigate('/admin')} className='hover:text-gray-400'>관리자</button>
+      <span className="text-gray-400 text-sm mx-1">|</span>
       <button onClick={handleLogout} className='hover:text-gray-400'>로그아웃</button>
     </header>
   );
