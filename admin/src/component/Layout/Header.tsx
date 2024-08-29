70fa69f58314a76721bbb9929e33fab8216bdc29
Kim Wongi <woongi3012@gmail.com>
2024-08-29 22:03:48 +0900

    style: 현재 탭 표시 스타일

--- admin/src/component/Layout/Header.tsx
+++ admin/src/component/Layout/Header.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { useAuth } from '../../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
   const { logout } = useAuth();
+  const location = useLocation();
 
   const handleLogout = () => {
     logout();
     navigate("/");
   };
 
+  const getButtonClass = (path: string) => 
+    location.pathname === path ? 'hover:text-gray-400 text-sky-400' : 'hover:text-gray-400';
+
   return (
     <header className="bg-gray-800 text-white px-4 py-2 flex justify-center space-x-8">
-      <button onClick={() => navigate('/notice')} className='hover:text-gray-400'>공지사항</button>
+      <button onClick={() => navigate('/notice')} className={getButtonClass('/notice')}>공지사항</button>
       <span className="text-gray-400 text-sm mx-1">|</span>
-      <button onClick={() => navigate('/lecture')} className='hover:text-gray-400'>강의</button>
+      <button onClick={() => navigate('/lecture')} className={getButtonClass('/lecture')}>강의</button>
       <span className="text-gray-400 text-sm mx-1">|</span>
-      <button onClick={() => navigate('/review')} className='hover:text-gray-400'>리뷰</button>
+      <button onClick={() => navigate('/review')} className={getButtonClass('/review')}>리뷰</button>
       <span className="text-gray-400 text-sm mx-1">|</span>
-      <button onClick={() => navigate('/user')} className='hover:text-gray-400'>사용자</button>
+      <button onClick={() => navigate('/user')} className={getButtonClass('/user')}>사용자</button>
       <span className="text-gray-400 text-sm mx-1">|</span>
-      <button onClick={() => navigate('/admin')} className='hover:text-gray-400'>관리자</button>
+      <button onClick={() => navigate('/admin')} className={getButtonClass('/admin')}>관리자</button>
       <span className="text-gray-400 text-sm mx-1">|</span>
       <button onClick={handleLogout} className='hover:text-gray-400'>로그아웃</button>
     </header>
