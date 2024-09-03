897bc6f2d37fac0c45f608c48ca4f8a9fa10eadd
Kim Wongi <woongi3012@gmail.com>
2024-09-03 20:03:30 +0900

    fix: deploy 오류 해결

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Main from './pages/Main';
-import Notice from './pages/Notice/Notice';
-import Lecture from './pages/Lecture/Lecture';
-import Review from './pages/Review/Review';
-import User from './pages/User/User';
-import Admin from './pages/Admin/Admin';
+import Login from './Page/Login/Login';
+import Main from './Page/Main';
+import Notice from './Page/Notice/Notice';
+import Lecture from './Page/Lecture/Lecture';
+import Review from './Page/Review/Review';
+import User from './Page/User/User';
+import Admin from './Page/Admin/Admin';
 import Layout from './component/Layout/Layout';
 import { useAuth } from './context/AuthContext';
-import NoticeDetail from './pages/Notice/NoticeDetail';
-import NoticeWrite from './pages/Notice/NoticeWrite';
-import NoticeEdit from './pages/Notice/NoticeEdit';
+import NoticeDetail from './Page/Notice/NoticeDetail';
+import NoticeWrite from './Page/Notice/NoticeWrite';
+import NoticeEdit from './Page/Notice/NoticeEdit';
 
 const App: React.FC = () => {
   const { isLoggedIn } = useAuth();
